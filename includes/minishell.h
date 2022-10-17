89e9e7bbf448108fef0314954e0d332b8810bf8f
Manuel SHARIFI <manuel.sharifi@gmail.com>
2022-10-17 20:26:13 +0200

    [MODIFY] .h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 14:31:25 by msharifi          #+#    #+#             */
-/*   Updated: 2022/10/17 17:52:32 by msharifi         ###   ########.fr       */
+/*   Updated: 2022/10/17 20:22:22 by msharifi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,24 +34,50 @@ typedef struct s_redir
 	struct s_redir	*next;
 }				t_redir;
 
+typedef struct s_data
+{
+	t_cmd	*cmd;
+	t_redir	*redir;
+}				t_data;
+
 // ********************* ALLOC_FREE *******************
 
 // alloc_free.c
-void		*ft_calloc(size_t n, size_t size);
-void		ft_free(void *addr);
+void	*ft_calloc(size_t n, size_t size);
+void	ft_free(void *addr);
+void	free_tab(char **tab);
+void	free_cmd(t_cmd *cmd);
+void	free_data(t_data *data);
 
-// ********************* UTILS *******************
+// ********************* CREATE ***********************
+
+// create.c
+t_data	*create_data(void);
+t_cmd	*create_cmd(t_cmd *cmd, char *str);
+
+// ********************* PRINT ************************
+
+// print.c
+void	print_tab(char **tab);
+void	print_cmd(t_cmd	*cmd);
+
+// ********************* UTILS ************************
 
 // list_utils.c
-t_cmd	*ft_lstnew(char	*av);
-t_cmd	*ft_lstlast(t_cmd *cmd_tbl);
-int			add_last(t_cmd *cmd_tbl, char *av);
+t_redir	*ft_lstnew_redir(void);
+t_cmd	*ft_lstnew_cmd(void);
+t_cmd	*ft_lstlast_cmd(t_cmd *cmd_tbl);
+t_cmd	*add_last_cmd(t_cmd *cmd, char *av);
 
-// split_utils.c
-int		redir(char *str);
+// slip_normal.c
+int		word_count_normal(char *str, char set);
+int		char_count_normal(char *str, char set, int pos);
+char	*ft_putword_normal(char *str, char *tab, char set, int pos);
+char	**ft_split_normal(char	*str, char set);
+
 
 // str_utils.c
-int			ft_strlen(char *str);
-int			ft_strchr(char *str, int c);
+int		ft_strlen(char *str);
+int		ft_strchr(char *str, int c);
 
 #endif
