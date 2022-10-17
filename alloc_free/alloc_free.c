1e3735b03d4bae3d88bf398bff2b5deb35553adc
Manuel SHARIFI <manuel.sharifi@gmail.com>
2022-10-17 20:28:50 +0200

    [ADD] free_data (pour tout free)

--- alloc_free/alloc_free.c
+++ alloc_free/alloc_free.c
@@ -6,7 +6,7 @@
 /*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 15:11:04 by msharifi          #+#    #+#             */
-/*   Updated: 2022/10/13 14:24:17 by msharifi         ###   ########.fr       */
+/*   Updated: 2022/10/17 20:12:07 by msharifi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,4 +33,47 @@ void	ft_free(void *addr)
 {
 	free(addr);
 	addr = NULL;
+}
+
+void	free_tab(char **tab)
+{
+	int	i;
+
+	i = 0;
+	while (tab[i])
+	{
+		ft_free(tab[i]);
+		i++;
+	}
+	if (tab)
+		ft_free(tab);
+}
+
+void	free_cmd(t_cmd *cmd)
+{
+	t_cmd *save;
+
+	save = cmd;
+	while (cmd)
+	{
+		cmd = cmd->next;
+		if (save->cmd_args)
+			free_tab(save->cmd_args);
+		if (save->cmd_path)
+			ft_free(save->cmd_path);
+		ft_free(save);
+		save = cmd;
+	}
+}
+
+// void	free_redir(t_redir *redir)
+// {
+
+// }
+
+void	free_data(t_data *data)
+{
+	if (data->cmd)
+		free_cmd(data->cmd);
+	ft_free(data);
 }
