285bcda50495a9a517857e93c5910abfc52874d5
Mateo FROISSARD <mfroissa@e2r11p21.clusters.42paris.fr>
2022-10-24 22:48:15 +0200

    [COMMIT] 24102022

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: mfroissa <mfroissa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 17:52:09 by mfroissa          #+#    #+#             */
-/*   Updated: 2022/10/19 10:56:17 by mfroissa         ###   ########.fr       */
+/*   Updated: 2022/10/20 19:13:53 by mfroissa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,18 +39,27 @@ int	count_words(char *str)
 				i++;
 			if (str[i] == '\0')
 				return (count);
-			if (is_in_charset(str[i + 1]) || str[i + 1] == '\0' ||
-				str[i + 1] == ' ' || str[i + 1] == '\t')
+			if (is_in_charset(str[i + 1]) || str[i + 1] == '\0'
+				|| str[i + 1] == ' ' || str[i + 1] == '\t')
 				count++;
 			i++;
 		}
-		if (is_in_charset(str[i]))
-		{
-			if (str[i] != '|' && str[i + 1] == str[i])
-				i++;
-			count++;
-			i++;
-		}
+		count += count_words_exp(str, &i);
+	}
+	return (count);
+}
+
+int	count_words_exp(char *str, int *i)
+{
+	int	count;
+
+	count = 0;
+	if (is_in_charset(str[(*i)]))
+	{
+		if (str[(*i)] != '|' && str[(*i) + 1] == str[(*i)])
+			(*i)++;
+		count++;
+		(*i)++;
 	}
 	return (count);
 }
@@ -74,76 +83,153 @@ int	count_chars(char *str, int n)
 				return (chars);
 			if (count == n)
 				chars++;
-			if (is_in_charset(str[i + 1]) || str[i + 1] == '\0' ||
-				str[i + 1] == ' ' || str[i + 1] == '\t')
+			if (is_in_charset(str[i + 1]) || str[i + 1] == '\0'
+				|| str[i + 1] == ' ' || str[i + 1] == '\t')
 				count++;
 			i++;
 		}
-		if (is_in_charset(str[i]))
-		{
-			if (count == n)
-				chars++;
-			if (str[i] != '|' && str[i + 1] == str[i])
-			{
-				chars++;
-				i++;
-			}
-			count++;
-			i++;
-		}
+		chars += count_chars_exp(str, &count, &n, &i);
 	}
 	return (chars);
 }
+
+int	count_chars_exp(char *str, int *count, int *n, int *i)
+{
+	int	chars;
+
+	chars = 0;
+	if (is_in_charset(str[(*i)]))
+	{
+		if ((*count) == (*n))
+			chars++;
+		if (str[(*i)] != '|' && str[(*i) + 1] == str[(*i)])
+		{
+			if ((*count) == (*n))
+				chars++;
+			(*i)++;
+		}
+		(*count)++;
+		(*i)++;
+	}
+	return (chars);
+}
+
+// char	*ft_putwords(char *str, int n, char *mot)
+// {
+// 	int	i;
+// 	int	count;
+// 	int	chars;
+
+// 	i = 0;
+// 	count = 0;
+// 	chars = 0;
+// 	while (str[i])
+// 	{
+// 		while (str[i] && !is_in_charset(str[i]))
+// 		{
+// 			while (str[i] && (str[i] == 32 || str[i] == '\t'))
+// 				i++;
+// 			if (str[i] == '\0')
+// 				break ;
+// 			if (count == n)
+// 			{
+// 				mot[chars] = str[i];
+// 				chars++;
+// 			}
+// 			if (is_in_charset(str[i + 1]) || str[i + 1] == '\0'
+// 				|| str[i + 1] == ' ' || str[i + 1] == '\t')
+// 				count++;
+// 			i++;
+// 		}
+// 		if (is_in_charset(str[i]))
+// 		{
+// 			if (count == n)
+// 			{
+// 				mot[chars] = str[i];
+// 				chars++;
+// 			}
+// 			if (str[i] != '|' && str[i + 1] == str[i])
+// 			{
+// 				i++;
+// 				if (count == n)
+// 				{
+// 					mot[chars] = str[i];
+// 					chars++;
+// 				}
+// 			}
+// 			count++;
+// 			i++;
+// 		}
+// 	}
+// 	mot[chars] = '\0';
+// 	return (mot);
+// }
 
 char	*ft_putwords(char *str, int n, char *mot)
 {
 	int	i;
 	int	count;
-	int	chars;
+	int	index;
 
 	i = 0;
 	count = 0;
-	chars = 0;
-	while (str[i])
+	while (str[i] && count != n)
 	{
-		while (str[i] && !is_in_charset(str[i]))
-		{
-			while (str[i] && (str[i] == 32 || str[i] == '\t'))
-				i++;
-			if (str[i] == '\0')
-				break;
-			if (count == n)
-			{
-				mot[chars] = str[i];
-				chars++;
-			}
-			if (is_in_charset(str[i + 1]) || str[i + 1] == '\0' ||
-				str[i + 1] == ' ' || str[i + 1] == '\t')
-				count++;
+		while (str[i] == ' ' || str[i] == '\t')
 			i++;
+		if (!is_in_charset(str[i]))
+		{
+			index = i;
+			count += ft_putwords_exp(str, &i);
+			if (count == n)
+				return (get_word(str, index, mot, count));
 		}
-		if (is_in_charset(str[i]))
+		else
 		{
+			index = i;
+			count += count_words_exp(str, &i);
 			if (count == n)
-			{
-				mot[chars] = str[i];
-				chars++;
-			}
-			if (str[i] != '|' && str[i + 1] == str[i])
-			{
-				i++;
-				if (count == n)
-				{
-					mot[chars] = str[i];
-					chars++;
-				}
-			}
-			count++;
-			i++;
+				return (get_word(str, index, mot, count));
 		}
 	}
-	mot[chars] = '\0';
+	return (get_word(str, index, mot, count));
+}
+
+char	*get_word(char *str, int index, char *mot, int n)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (i < index)
+		i++;
+	while (i < index + count_chars(str, n))
+	{
+		mot[j] = str[i];
+		i++;
+		j++;
+	}
+	mot[j] = '\0';
 	return (mot);
+}
+
+int	ft_putwords_exp(char *str, int *i)
+{
+	int	count;
+
+	count = 0;
+	while (str[(*i)] && !is_in_charset(str[(*i)]))
+	{
+		if (is_in_charset(str[(*i) + 1]) || str[(*i) + 1] == '\0'
+			|| str[(*i) + 1] == ' ' || str[(*i) + 1] == '\t')
+		{
+			count++;
+			return (count);
+		}
+		(*i)++;
+	}
+	return (count);
 }
 
 char	**ft_split(char *str, t_data *data)
@@ -158,7 +244,7 @@ char	**ft_split(char *str, t_data *data)
 	printf("words : %d\n", count_words(str));
 	while (i < count_words(str))
 	{
-		printf("chars %d : %d\n", i, count_chars(str, i) + 1);
+		printf("chars %d : %d\n", i, count_chars(str, i));
 		tab[i] = ft_calloc(sizeof(char), count_chars(str, i) + 1);
 		tab[i] = ft_putwords(str, i, tab[i]);
 		add_to_list(data, tab[i]);
