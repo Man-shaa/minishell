2f2db5b2ae515a5b9202e15761298390dc1f5e46
Manuel SHARIFI <msharifi@e2r10p2.clusters.42paris.fr>
2022-11-16 19:18:18 +0100

    balc

--- main_manu.c
+++ main_manu.c
@@ -6,7 +6,7 @@
 /*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 10:08:53 by mfroissa          #+#    #+#             */
-/*   Updated: 2022/11/15 16:22:38 by msharifi         ###   ########.fr       */
+/*   Updated: 2022/11/16 16:13:23 by msharifi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 // ft_atoi avant pour recup d'argument du prompt (avec tab et 32)
 
-// int	main(int ac, char **av, char **envp)
-// {
-// 	t_data	*data;
+int	main(int ac, char **av, char **envp)
+{
+	t_data	*data;
 
-// 	(void)ac;
-// 	data = create_data(envp);
-// 	if (!data)
-// 		return (1);
-// 	ft_split(av[1], data);
-// 	print_list(data->list);
-// 	free_data(data);
-// 	return (0);
-// }
+	(void)ac;
+	data = create_data(envp);
+	if (!data)
+		return (1);
+	ft_split(av[1], data);
+	print_list(data->list);
+	free_data(data);
+	return (0);
+}
