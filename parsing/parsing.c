c3d282854738afafe4f116ecc9df2c33626b1b25
Mateo FROISSARD <mfroissa@e2r11p21.clusters.42paris.fr>
2022-11-17 18:09:47 +0100

    split OK, check dup OK, changement du nom parsing en split, mkdir parsing(check dup)

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: mfroissa <mfroissa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 17:15:26 by mfroissa          #+#    #+#             */
-/*   Updated: 2022/11/17 17:54:16 by mfroissa         ###   ########.fr       */
+/*   Updated: 2022/11/17 18:07:35 by mfroissa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ int	check_dup(t_data *data)
 	tmp = data->list;
 	while (tmp)
 	{
-		if (tmp->type != 1 && tmp->next->type != 1)
+		if (tmp->next && tmp->type != 1 && tmp->next->type != 1)
 		{
 			if (tmp->next->type != 6)
 				return (0);
 		}
-		if (tmp->type == 6 && tmp->next->type == 6)
+		if (tmp->next && tmp->type == 6 && tmp->next->type == 6)
 			return (0);
 		tmp = tmp->next;
 	}
