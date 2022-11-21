f6924f1de2f732ae9b5c7cbdce9292fcf42ec515
Manuel SHARIFI <msharifi@e2r10p2.clusters.42paris.fr>
2022-11-21 10:53:58 +0100

    sans add

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,13 +6,11 @@
 /*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 17:39:45 by msharifi          #+#    #+#             */
-/*   Updated: 2022/11/10 20:53:40 by msharifi         ###   ########.fr       */
+/*   Updated: 2022/11/21 01:32:10 by msharifi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // gerer option -n (newline)
-// faut il faire qq chose de particulier pour ecrire le contienu d'un fichier
-// (echo [nom_du_fichier]) ou c'es gerer tout seul par l'execve ? 
 
 #include "../includes/minishell.h"
 
