4d424fee84325cc0618837a3dba6e18120309446
Manuel SHARIFI <msharifi@e2r10p2.clusters.42paris.fr>
2022-12-20 21:55:01 +0100

    [ADD] [IN PROGRESS] exec/pipe.c + redirections.c

---
+++ exec/pipe.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipe.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/20 20:38:01 by msharifi          #+#    #+#             */
+/*   Updated: 2022/12/20 21:27:50 by msharifi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+int	create_pipes(t_data *data)
+{
+	int		i;
+	t_proc	*proc;
+
+	proc = data->proc;
+	i = 0;
+	proc->n_pipes = pipe_count(data->cmd) - 1;
+	if (proc->n_pipes <= 0)
+		return (1);
+	proc->pipe_fd = ft_calloc(2 * proc->n_pipes, sizeof(int));
+	if (!proc->pipe_fd)
+		return (0);
+	while (i < proc->n_pipes)
+	{
+		if (pipe(proc->pipe_fd + 2 * i) < 0)
+			return (err_msg("Pipe failed", NULL, NULL, 0));
+		i++;
+	}
+	return (1);
+}
+
+void	close_pipes(t_proc *proc)
+{
+	int	i;
+
+	i = 0;
+	close(proc->fd_in);
+	close(proc->fd_out);
+	while (i < proc->n_pipes)
+	{
+		close(proc->pipe_fd[i]);
+		i++;
+	}
+}
+
---
+++ exec/redirections.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirections.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/20 20:39:51 by msharifi          #+#    #+#             */
+/*   Updated: 2022/12/20 21:31:56 by msharifi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+// open et dup2 a l'interieur de chaque pipe si il y a des redirections < > >>
+// Return 1 si tout s'est bien passe, sinon 0
+int	handle_redir(t_data *data, t_cmd *cmd)
+{
+	int	i;
+
+	i = 0;
+	if (!cmd->token)
+		return 0;
+	while (cmd->token[i])
+	{
+		if (!open_fd(data->proc, cmd->token[i], cmd->type[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	open_fd(t_proc *proc, char *token, int type)
+{
+	if (type == 2)
+	{
+		proc->fd_in = open(token, O_RDONLY, 0644);
+		if (proc->fd_in < 0)
+			return (err_msg("open: No such file or directory", NULL, NULL, 1));
+		dup2(proc->fd_in, STDIN_FILENO);
+		close(proc->fd_in);
+	}
+	else if (type == 3)
+	{
+		proc->fd_out = open(token, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (proc->fd_out < 0)
+			return (err_msg("open: No such file or directory", NULL, NULL, 2));
+		dup2(proc->fd_out, STDOUT_FILENO);
+		close(proc->fd_in);
+	}
+	else if (type == 5)
+	{
+		proc->fd_out = open(token, O_WRONLY | O_CREAT | O_APPEND, 0644);
+		if (proc->fd_out < 0)
+			return (err_msg("open: No such file or directory", NULL, NULL, 2));
+		dup2(proc->fd_out, STDOUT_FILENO);
+		close(proc->fd_in);
+	}
+	return (1);
+}
+
+void	double_dup2(int fd1, int fd2)
+{
+	dup2(fd1, STDIN_FILENO);
+	dup2(fd2, STDOUT_FILENO);
+}
+
+void	redirect_pipe(t_proc *proc)
+{
+	int	pipe_index;
+
+	pipe_index = 0;
+	if (proc->n_pipes <= 0)
+		return ;
+	while (pipe_index < proc->n_pipes)
+	{
+		if (pipe_index == 0)
+			double_dup2(proc->fd_in, proc->pipe_fd[1]);
+		else if (pipe_index == proc->n_pipes - 1)
+			double_dup2(proc->pipe_fd[2 * pipe_index - 2], proc->fd_out);
+		else
+			double_dup2(proc->pipe_fd[2 * pipe_index - 2],
+				proc->pipe_fd[2 * pipe_index + 1]);
+		close_pipes(proc);
+	}
+}
