ad143f2d4fa479599f62b724cf0cf92b3a97e253
Manuel SHARIFI <msharifi@e2r10p2.clusters.42paris.fr>
2023-01-09 17:50:17 +0100

    [FIX] unset plusieurs node a la fois (char ** au lieu de char *)

--- builtins/unset.c
+++ builtins/unset.c
@@ -6,30 +6,38 @@
 /*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 18:02:59 by msharifi          #+#    #+#             */
-/*   Updated: 2022/12/21 18:22:06 by msharifi         ###   ########.fr       */
+/*   Updated: 2023/01/09 17:48:54 by msharifi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-// Retire le node str dans t_envp s'il existe, sinon ne fait rien
-void	ft_unset(t_data *data, char *str)
+// Retire les node du tableau de string args dans t_envp s'ils existent,
+// sinon ne fait rien
+void	ft_unset(t_data *data, char **args)
 {
+	int		i;
 	t_envp	*before;
 	t_envp	*node;
 
-	if (!str || !str[0])
+	i = 0;
+	if (!args || !args[0])
 		return ;
-	before = data->envp;
-	node = search_node(data->envp, str);
-	if (!node)
-		return ;
-	while (before->next && !ft_strcmp(before->next->tab[0], str))
-		before = before->next;
-	if (!before->next)
-		data->envp = node->next;
-	else
-		before->next = node->next;
-	free_tab(node->tab);
-	ft_free(node);
+	while (args[i])
+	{
+		before = data->envp;
+		node = search_node(data->envp, args[i]);
+		if (node)
+		{
+			while (before->next && !ft_strcmp(before->next->tab[0], args[i]))
+				before = before->next;
+			if (!before->next)
+				data->envp = node->next;
+			else
+				before->next = node->next;
+			free_tab(node->tab);
+			ft_free(node);
+		}
+		i++;
+	}
 }
