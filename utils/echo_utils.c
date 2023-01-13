c3c5855136abd0288147dcd409e34563bac8b8cd
Manuel SHARIFI <msharifi@e2r10p2.clusters.42paris.fr>
2023-01-13 22:28:23 +0100

    [PULL]

--- utils/echo_utils.c
+++ utils/echo_utils.c
@@ -6,7 +6,7 @@
 /*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 15:27:16 by msharifi          #+#    #+#             */
-/*   Updated: 2023/01/09 18:09:39 by msharifi         ###   ########.fr       */
+/*   Updated: 2023/01/13 22:28:03 by msharifi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ int	echo_env_var(t_data *data, char *str)
 	else
 	{
 		res = ignore_charset(str + 1, "(){}", 0);
-		node = data->envp;
+		node = data->envp;z
 		node = search_node(data->envp, res);
 		if (!node)
 		{
