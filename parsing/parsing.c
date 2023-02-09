3d2761be52da15ce5a7594ac2f7b80f57c7099f6
Mateo FROISSARD <mfroissa@e2r11p21.clusters.42paris.fr>
2023-02-09 16:12:36 +0100

    [FIX] check pipe

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: mfroissa <mfroissa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 17:15:26 by mfroissa          #+#    #+#             */
-/*   Updated: 2023/02/09 15:21:03 by mfroissa         ###   ########.fr       */
+/*   Updated: 2023/02/09 16:12:13 by mfroissa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int check_pipe(t_data *data)
 	t_list	*tmp;
 
 	tmp = data->list;
-	if (tmp->type == 6)
+	if (tmp && tmp->type == 6)
 		return (0);
 	return (1);
 }
