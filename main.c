4c3b9c6cf143ef26c9acdea481fbaf99bbfad9b6
Manuel SHARIFI <msharifi@e2r10p2.clusters.42paris.fr>
2023-02-17 22:07:04 +0100

    [UPDATE] probleme restant dans le main a jour

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mfroissa <mfroissa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 14:44:22 by msharifi          #+#    #+#             */
-/*   Updated: 2023/02/17 18:33:24 by mfroissa         ###   ########.fr       */
+/*   Updated: 2023/02/17 22:06:09 by msharifi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,11 @@
 
 int	g_return_val;
 
-// Plusieurs here_doc dans 2 pipe separes
-
 // valeur de retour de CTRL-\ pas bon (0 au lieu de 131)
-// valeur de retour de CTRL-C pas bon (0 au lieu de 130)
 
 // echo "$9HOME" OU "echo $:$=" essaye d'expand alors qu'il faut pas
 
-// "|" conditional jump
-
-// "export Hola=bonjour" PUIS "export Hola" Hola doit plus avoir "bonjour" :(
-// qq part dans replace_value je pense
-
-// "exit 99999999999999999999999999999" compare avec bash
+// echo yo > bonjour fd 1 open
 
 int	main(int ac, char **av, char **envp)
 {
