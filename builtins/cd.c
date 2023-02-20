6948e23e7acc525db9cc243d16fb052a59db2218
Manuel SHARIFI <msharifi@e2r10p2.clusters.42paris.fr>
2023-02-20 23:08:44 +0100

    [FIX] return value de ft_cd + message d'erreur en double

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 05:07:52 by msharifi          #+#    #+#             */
-/*   Updated: 2023/02/20 22:56:50 by msharifi         ###   ########.fr       */
+/*   Updated: 2023/02/20 23:08:04 by msharifi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,35 @@ int	check_cd(char **args, int m)
 	int	i;
 
 	i = 0;
-	if (!args[0] && m == 1)
-		return (err_msg("minishell: no relative or absolute path", 0, 0, 0));
+	if (!args[0])
+	{
+		if (m == 1)
+			err_msg("minishell: no relative or absolute path", 0, 0, 1);
+		return (1);
+	}
 	while (args[i])
 		i++;
-	if (i > 1 && m == 1)
-		return (err_msg("minishell: more than 1 relative or absolute path",
-				NULL, NULL, 0), 0);
-	if (!is_a_dir(args[0]) && m == 1)
-		return (err_msg("minishell: ", args[0], ": is not a directory", 0));
-	return (1);
+	if (i > 1)
+	{
+		if (m == 1)
+			err_msg("minishell: too many arguments", NULL, NULL, 1);
+		return (1);
+	}
+	if (!is_a_dir(args[0]))
+	{
+		if (m == 1)
+			err_msg("minishell: ", args[0], ": is not a directory", 1);
+		return (1);
+	}
+	return (0);
 }
 
 // changer oldpwd et pwd avec replace...
 int	ft_cd(t_data *data, char **args, int m)
 {
-	if (!check_cd(args, m))
+	g_return_val = check_cd(args, m);
+	printf("%d\n\n\n", g_return_val);
+	if (g_return_val)
 		return (1);
 	chdir(args[0]);
 	replace_pwd_my_env(data->envp);
